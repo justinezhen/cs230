31e442323a6aab6315711dc5f8cb409fa6fa74d4
justinezhen <95762318+justinezhen@users.noreply.github.com>
2021-12-08 10:17:38 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -10,7 +10,7 @@ I have not given my code to any student.
 
 
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import streamlit as st
 import numpy as np
 import pydeck as pdk
